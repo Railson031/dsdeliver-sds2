01efe153b95502d71caf7dc8217b26a3e3e3e602
railson carvalho <railson031@hotmail.com>
2021-01-10 12:58:54 -0300

    sds-02

--- src/Orders/OrderSummary.tsx
+++ src/Orders/OrderSummary.tsx
@@ -2,7 +2,14 @@ function OrderSummary() {
     return (
         <div className="order-summary-container">
             <div className="order-summary-content">
-
+                <span className="amount-selected-container">
+                <strong className="amount-selected">2</strong>
+                PEDIDOS SELECIONADOS
+                </span>
+                <span className="order-summary-total">
+                    <strong className="amount-selected">R$ 50,00</strong>
+                    VALOR TOTAL
+                </span>
             </div>
         </div>
     )
